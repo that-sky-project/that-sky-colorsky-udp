7f65579c7097710c486ce688b1d0995f9d044f4c
Sky <i@colorsky.fun>
2026-08-14 18:13:23 +0800

    fix: netrpc modify

--- src/server/game_msg/net_rpc.rs
+++ src/server/game_msg/net_rpc.rs
@@ -12,8 +12,17 @@ use crate::{
 
 impl crate::server::state::ServerState {
     /// Handles net rpc
-    pub(super) fn handle_net_rpc(&mut self, peer_key: usize, msg: GameMsg) -> Option<()> {
+    pub(super) fn handle_net_rpc(&mut self, peer_key: usize, mut msg: GameMsg) -> Option<()> {
         // send to all
+        let netrpc = &mut msg.payload[1..];
+        if netrpc.starts_with(&[0xb3, 0xb9, 0xd9, 0x20, 0x00, 0x24, 0x00]) {
+            let sender = self.peers.get(&peer_key)?;
+
+            if (netrpc[9] == 1) {
+                netrpc[18..34].copy_from_slice(&sender.uuid.raw());
+            }
+        }
+
         tracing::debug!("[NET_RPC] hex={}", hex_preview(&msg.payload, 64));
         self.relay_game_msg(GameMsgId::NetRpc, &msg.payload, peer_key)
     }
